87ccb531342e502bbf91a713b93ee96840ef6d15
SergioDonnet <donnet7534@gmail.com>
2024-02-08 08:07:45 +0000

    se crea primera pagina sin diseno solo html

--- ThinKids/src/app/app.component.ts
+++ ThinKids/src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ProgressbarComponent } from './components/progressbar/progressbar.component';
+import {MatButtonModule} from '@angular/material/button';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,ProgressbarComponent],
+  imports: [RouterOutlet,ProgressbarComponent,MatButtonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
